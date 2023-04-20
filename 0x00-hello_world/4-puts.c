77b70ac124480419be469584c70def37395c7ba6
TelezaKanthonga <telezakanthonga@gmail.com>
2023-04-20 05:13:16 -0700

    a C program that prints exactly Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
-/** 
- * Done on 20 April 2023
- * by Teleza Kanthonga
- * alx software engineering 
+/*
+ * By Teleza Kanthonga
+ * Fie type : C
+ * alx software engineering
+ * 20 April 2023
  */
+
 #include <stdio.h>
 
 /**
- *This is the 
- * main function
+ * This is the  main function 
+ * main cout: Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
