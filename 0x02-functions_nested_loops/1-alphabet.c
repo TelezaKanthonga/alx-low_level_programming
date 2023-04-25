db8c38114ea53a0862fd10db77ed350cf7d7702d
TelezaKanthonga <telezakanthonga@gmail.com>
2023-04-25 04:13:27 -0700

    first commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - Prints the English alphabet from a to z
- * Description: Uses _putchar to print each letter of the alphabet, followed by a new line
+ * 
  * Return: Nothing
  */
 void print_alphabet(void)
