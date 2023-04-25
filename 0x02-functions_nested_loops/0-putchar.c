8d96525d4b302f97de30c2ee272699645102d981
TelezaKanthonga <telezakanthonga@gmail.com>
2023-04-25 03:32:34 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,24 @@
-#include <stdio>
+/**
+ *
+ * by Teleza
+ * 25th April 2023
+ */
+#include 'main.h'
+#include <stdio.h>
 
+/**
+ * main function
+ * Return: Always 0
+ */
 int main(void){
-	_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+         _putchar('r');
 		        
           return 0;
 }
