fc47eb6869f796826b01c983c49c63433ed2682d
TelezaKanthonga <telezakanthonga@gmail.com>
2023-04-25 04:33:31 -0700

    a function that prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * print_alphabet_x10 - Prints the English alphabet from a to z "Uses _putchar to print each letter of the alphabet," 10 times.
  *
@@ -7,15 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char c;
+        int i;
+        char letter;
 
-	for (i = 0; i < 10; ++i)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
-	}
+        for (i = 0; i < 10; ++i)
+        {
+                for (letter = 'a'; letter <= 'z'; letter++)
+                {
+                        _putchar(letter);
+                }
+                _putchar('\n');
+        }
 }
