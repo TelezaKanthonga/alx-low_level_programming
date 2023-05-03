3db4955e24fea9821e840cf0c69dc2089528ca06
TelezaKanthonga <telezakanthonga@gmail.com>
2023-05-03 13:02:11 -0700

    a program that generates random valid passwords for the program

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
-#include <stdli.h>
+#include <stdlib.h>
 #include <time.h>
-
 /**
- * main - Entry point and generates a random password of length 100
- * Return: Always 0 (Success)
+ * main - Entry point and generate random passwords of 100 length
+ * Return : Always 0
  */
 int main(void)
 {
     int i;
     char password[100];
-    char expected_password[] = "Tada! Congrats";
     srand(time(NULL));
     for (i = 0; i < 100; i++)
     {
         password[i] = rand() % 94 + 33;
     }
-    if (strcmp(password, expected_password) == 0)
-    {
-        printf("Tada! Congrats\n");
-    }
-    else
-    {
-        printf("Wrong password\n");
-    }
+    printf("%s\n", password);
 
     return (0);
 }
